b772232d50b0e5e7cb274385e88e0253edc52fb7
jclayton128 <jclayton128@gmail.com>
2021-07-30 14:49:00 -0400

    WordValidater, with word checking functionality, using Collins Scrabble Words 2019.

---
+++ Assets/Scripts/Utility/WordValidater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WordValidater : MonoBehaviour
+{
+    [SerializeField] TextAsset wordListRaw = null;
+    string wordList;
+    DebugHelper dh;
+    void Start()
+    {
+        dh = FindObjectOfType<DebugHelper>();
+        wordList = wordListRaw.ToString();
+        dh.DisplayDebugLog($"Loaded {wordList.Length} words");
+    }
+
+    public bool CheckWordValidity(string testWord)
+    {
+        if (wordList.Contains(testWord))
+        {
+            Debug.Log($"{testWord} is valid");
+            dh.DisplayDebugLog($"{testWord} is valid");
+            return true;
+        }
+        else
+        {
+            Debug.Log($"{testWord} is invalid");
+            dh.DisplayDebugLog($"{testWord} is invalid");
+            return false;
+        }
+    }
+}
