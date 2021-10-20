cd559085a70f41152199687da6f75ac962d93534
jclayton128 <jclayton128@gmail.com>
2021-10-19 23:14:25 -0400

    Began rework on jewels to provide better feedback when insufficient energy.

--- Assets/JewelManager.cs
+++ Assets/JewelManager.cs
@@ -7,14 +7,16 @@ using UnityEngine.UI;
 public class JewelManager : MonoBehaviour
 {
     [SerializeField] Image[] jewelImages = null;
-    [SerializeField] GameObject[] chargedJewelParticleFXs = null;
+    [SerializeField] ParticleSystem jewel0PS = null;
 
     //state
-    int jewelsInstalled;
+    int jewelsInstalled = 1;
 
     private void Start()
     {
-        jewelsInstalled = jewelImages.Length;
+        //jewelsInstalled = jewelImages.Length;
+        ModifyJewelCount(3);
+
     }
 
     public void UpdateJewelImage(float factor)
@@ -24,6 +26,39 @@ public class JewelManager : MonoBehaviour
         {
             float specificFactor = Mathf.Clamp01((factor - (subamount * i)) / subamount);
             jewelImages[i].color = Color.HSVToRGB(0, specificFactor, 1);
+            if (i == 0 && specificFactor < 1)
+            {
+                Debug.Log("insuff");
+                ProvideFeedbackAboutInsufficientEnergy(true);
+            }
+            if (i == 0 && specificFactor >= 1)
+            {
+                Debug.Log("plenty");
+                ProvideFeedbackAboutInsufficientEnergy(false);
+            }
+        }
+    }
+
+    public void ModifyJewelCount(int newJewelCount)
+    {
+        jewelsInstalled = newJewelCount;
+        foreach (var jewel in jewelImages)
+        {
+            jewel.color = Color.clear;
+        }
+    }
+
+    public void ProvideFeedbackAboutInsufficientEnergy(bool isInsufficient)
+    {
+        if (isInsufficient)
+        {
+            ParticleSystem.EmissionModule em = jewel0PS.emission;
+            em.rateOverTime = 6;
+        }
+        else
+        {
+            ParticleSystem.EmissionModule em = jewel0PS.emission;
+            em.rateOverTime = 0;
         }
     }
 }
