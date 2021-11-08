82202fb7c97e44969ea470698b1d1ddc66993092
jclayton128 <jclayton128@gmail.com>
2021-11-08 16:38:22 -0500

    UI refactor part 1

---
+++ Assets/Scripts/UI_Panel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UI_Panel : MonoBehaviour
+{
+    abstract public void ShowHideElements(bool shouldBeShown);
+}
